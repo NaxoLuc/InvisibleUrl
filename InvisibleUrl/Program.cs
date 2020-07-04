29acd09f52c1119db40c0e085b0007400eea5529
NaxoLuc <NaxoLuc@gmail.com>
2020-07-04 13:20:20 +0200

    Add project files.

---
+++ InvisibleUrl/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Net;
+
+namespace InvisibleUrl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          Get(args[0]);
+        }
+
+        //http GET request
+        static string Get(string uri)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
+            request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            using (Stream stream = response.GetResponseStream())
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                return reader.ReadToEnd();
+            }
+        }
+    }
+}
